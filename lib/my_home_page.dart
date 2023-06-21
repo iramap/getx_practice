e3dd0c97de7b80a9ede88ca47f76f4f788087ff7
Ramprasad Prajapati <ramaprajapati95@gmail.com>
2023-06-21 19:45:32 +0530

    first commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Column(
+          children: [
+            Text("Hello Flutter"),
+            Text("Hello Flutter"),
+          ],
+        ),
+      ),
+    );
+  }
+}
